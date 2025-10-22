fe2c2c2ddd04e1ab434e69ad153941c2d3a629d6
hoangtucuagio <hoangtucuagio@gmail.com>
2025-10-22 17:55:14 +0700

    feat: add train and eval files

---
+++ scripts/session_1/training.py
@@ -0,0 +1,9 @@
+from typing import Any
+def training(request: Any) -> Any:
+    """
+    A placeholder function for training a model.
+
+    Args:
+        data (Any): The training data.
+    """
+    pass
